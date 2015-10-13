0ab626bcec4b91c5697353ac9d13800982579a2a
william-isley-3rd <william.isley.3rd@gmail.com>
2015-10-13 15:46:40 -0500

    Update G09-Quasiharmonic-Freq-Corrector.pl

--- G09-Quasiharmonic-Freq-Corrector.pl
+++ G09-Quasiharmonic-Freq-Corrector.pl
@@ -7,8 +7,6 @@
 #                                                                                          #
 # AUTHORS :: William Christian Isley III                                                   #
 #                                                                                          #
-# DISCLAIMER :: This script is provided "as-is", without any warranties or support.        #
-# The authors assume no liability or responsiblity for the use of this software.           #
 #                                                                                          #
 # SOFTWARE :: This script was created using Perl v 5.20                                    #
 # This script has dependency on the pacakges, Getopt::Long                                 #
@@ -42,7 +40,15 @@
 # Update v1.1: Change log: updated the reporting of variables to correctly reflect gibbs,  #
 # enthalpy and entropy                                                                     #
 #                                                                                          #
+# DISCLAIMER :: This program is free software: you can redistribute it and/or modify       #
+# it under the terms of the GNU General Public License as published by the Free Software   #
+# Foundation, either version 3 of the License, or any later version                        #
+# This program is distributed in the hope that it will be useful, but WIHOUT ANY WARRANTY  #
+# without even the implied warranty of MERCANTABILITY or FITNESS FOR A PARTICULAR PURPOSE  #
+# See the GNU General Public License for more details.                                     #
+#                                                                                          #
 ############################################################################################
+
 
 
 use strict;
